31c9c385c7536b2e7d0849b46386ea5cde61565f
nagasai <nagasaij@iisc.ac.in>
2025-07-19 19:46:23 +0530

    Added request_id, attendees, final_start_time and final_end_time fields to the class Event

--- src/classes.py
+++ src/classes.py
@@ -1,14 +1,25 @@
+from datetime import datetime
+
 class Event:
-    def __init__(self, creator, start_time, end_time, subject, content):
+    def __init__(self, request_id: int, creator: str, start_time: datetime,
+                 end_time: datetime, subject: str, content: str,
+                 attendees: list[str]):
+        
+        # Fields initialized at the creation of the event
+        self.request_id = request_id
         self.creator = creator
         self.start_time = start_time
         self.end_time = end_time
         self.subject = subject
         self.content = content
+        self.attendees = attendees
+
+        # Fields initialized later in the pipeline
         self.window_start_time = None
         self.window_end_time = None
         self.priority = None
-    # finish the rest as needed
+        self.final_start_time = None
+        self.final_end_time = None
 
     def to_dict(self):
         return {
